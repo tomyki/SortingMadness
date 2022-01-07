2c5e31f078f3aed8cbc961d9f5bdddbd8f571813
unknown <tomaszmanys@gmail.com>
2022-01-07 20:35:34 +0100

    mockito

--- src/test/java/pl/put/poznan/transformer/logic/SortingMadnessTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/SortingMadnessTest.java
@@ -11,7 +11,11 @@ class SortingMadnessTest {
     @Test
     public void testMock() throws Exception {
         SortingMadnessController mockObject = mock(SortingMadnessController.class);
-        when(mockObject.post(anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), new String[] {anyString()})).thenReturn("cokolwiek");
-
+        SortingMadness obiekt = new SortingMadness(new String[] {"anyString()"});
+        String[] test = new String[] {"anyString()"};
+        when(mockObject.post(anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(),any(String[].class))).thenReturn(obiekt.transform("2,1,3","a","int","0","b","desc","0"));
+        String wynik = mockObject.post("a","a","a","a","a","a","a",test);
+        verify(mockObject);
+        assertEquals(wynik, "[3, 2, 1]");
     }
 }
